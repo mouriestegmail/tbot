e54d8389eacebe2b2d5f1308360eeba24bae78a6
w1 <w1@bot.com>
2024-07-16 21:29:19 +0400

    log ah

--- main.py
+++ main.py
@@ -93,6 +93,58 @@ async def create_command(chat_id, context: ContextTypes.DEFAULT_TYPE, text="") -
     else:
         await context.bot.send_message(chat_id=chat_id, text=f"command already exist")
 
+async def make_sum(chat_id, context: ContextTypes.DEFAULT_TYPE, count=15, full=False) -> None:
+    global log_dir
+    current_time = datetime.now()
+    text = ""
+    print("make_sum")
+    ss = 0
+    for i in range (1,5):
+        try:
+            filename = log_dir + f'\\{current_time.strftime("%d.%m.%Y")}_W{i}.sold'
+            print(filename)
+            with open(filename, 'r') as file:
+                first_line = file.readline().strip()
+                apples = eval(first_line)    
+                apples = dict(sorted(apples.items()))
+                s = 0
+                for k,v in apples.items():
+                    s += v
+                ss += s
+                if first_line:
+                    text += "\n" + str(apples).replace(" ", "") + f' {s}'
+        except Exception as e:
+            text += "\n err"
+
+
+    text += f"\n {ss} \nAH:\n"
+
+    for i in range (1,5):
+        try:
+            filename = log_dir + f'\\W{i}.ah'
+            print(filename)
+            with open(filename, 'r') as file:
+                first_line = file.readline().strip()
+                apples = eval(first_line)    
+                apples = dict(sorted(apples.items()))
+                s = 0
+                for k,v in apples.items():
+                    s += v
+                ss += s
+                if first_line:
+                    text += "\n" + str(apples).replace(" ", "") + f' {s}'
+        except Exception as e:
+            text += "\n err"    
+
+    print(text)
+    filename = log_dir + f'\\money.txt'
+    print(filename)
+    with open(filename, 'r') as file:
+        text += "".join(list(file.readlines()))
+    text = "```log\n" + text + "\n```"
+    print(text)
+    await context.bot.send_message(chat_id=chat_id, text=text, parse_mode='Markdown')
+
 
 async def make_log(chat_id, context: ContextTypes.DEFAULT_TYPE, count=15, full=False) -> None:
     global log_dir
@@ -181,7 +233,9 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     elif "comm" in text:
         print(text)
         await create_command(chat_id, context, text)
-
+    elif "sum" in text:
+        print(text)
+        await make_sum(chat_id, context, text)
     # await update.message.reply_text("нажми на кнопку :)", reply_markup=markup, )
 
     if flag_alarm:
