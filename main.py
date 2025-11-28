70f1d1212830f543d5d10bd233183ed4df2b88ff
Andrey V. Sokolov <andrey.v.sokolov@ringcentral.com>
2025-11-28 21:48:02 +0200

    worker.json

--- main.py
+++ main.py
@@ -115,8 +115,7 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         await make_sum(chat_id, context)
 
     elif "setbconf" in text:
-        if config.mode == config.mode_buyer:
-            await set_bconf(chat_id, context, text)
+        await set_bconf(chat_id, context, text)
 
     elif "bconf" in text:
         await make_bconf(chat_id, context)
