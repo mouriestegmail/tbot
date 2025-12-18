a6fe4d0639a52839d6566b218a1c0a639862dcef
Andrey V. Sokolov <andrey.v.sokolov@ringcentral.com>
2025-12-18 13:50:43 +0200

    reaction

--- file_handler.py
+++ file_handler.py
@@ -92,12 +92,11 @@ class NewFileHandler(FileSystemEventHandler):
             chat_id = int(chat_id_str)
             for i in 2,3,4,5,6:
                 try:
-                    text += f"лайк, если хочешь сохранить сообщение \n {1}"
+                    text += f"реплай, что бы сохранить \n [{1}]"
                     await self.bot.send_message(chat_id=chat_id, text=text, parse_mode='Markdown')
                     return
                 except Exception as e:
-                    print(f"[Error notify] reply: {e}")
-                    text += f"-> {i}"
+                    text += f"-> [{i}]"
                     await asyncio.sleep(0.5)
 
 
